c9478fc05f62b243fac2cce6974f9d762eade17f
AlvaroDaniel20 <adquinterop@ufpso.edu.co>
2024-09-03 12:54:10 -0500

    Actualizacion 1.0.04

--- javascript.js
+++ javascript.js
@@ -2,9 +2,12 @@ function verificarcode() {
     const codigoing = document.getElementById('code').value;
 
     const codigocorr = "2024"
+    const codigocorr2 = "7896"
 
     if (codigoing === codigocorr) {
         window.location.href = 'https://youtu.be/keyRM3h_7tk?feature=shared';
+    } else if (codigoing === codigocorr2) {
+        window.location.href = 'https://youtu.be/mY-9t2iLi4Q?si=5nCRu4euc2Yhac-V'
     } else {
         alert('Esta Mal');
     }
